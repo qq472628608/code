604e25cb23e5295ad9dcb2b0b33322856039cdbc
root <472628608@qq.com>
2020-05-28 09:39:21 +0800

    remove graduation_project

--- graduation_project/src/main/java/com/fjw/service/IAttributeValueService.java
+++
@@ -1,9 +0,0 @@
-package com.fjw.service;
-
-import java.util.List;
-
-import com.fjw.domain.AttributeValue;
-
-public interface IAttributeValueService {
-	public List<AttributeValue> list(Long sa_id);
-}
--- graduation_project/src/main/java/com/fjw/service/IKeyValueProductService.java
+++
@@ -1,7 +0,0 @@
-package com.fjw.service;
-
-
-public interface IKeyValueProductService {
-	public void add(Long[] kvs,Long product_id);
-	public void delete(Long id);
-}
